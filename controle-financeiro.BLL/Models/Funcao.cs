8e028a7b0cb6d7b46ba939e5092e703f4f12d4de
c1319055 <c1319055@interno.bb.com.br>
2021-12-06 09:10:32 -0300

    [UPT] [TASK-?????] Finalização do controle financeiro, com a API e Angular integrado

--- controle-financeiro.BLL/Models/Funcao.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace controle_financeiro.BLL.Models
-{
-    public class Funcao : IdentityRole<string>
-    {
-        public int FuncaoId { get; set; } 
-    }
-}
